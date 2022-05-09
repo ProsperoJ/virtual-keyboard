484e6362039380d368972f5b595bb953405374e4
Alexey Azarko <azarkoav@gmail.com>
2022-05-09 11:42:39 +0300

    feat: add eslint

--- scripts/index.js
+++ scripts/index.js
@@ -30,7 +30,6 @@ class Keyboard {
 
     this.keys = this.keysContainer.querySelectorAll('.keyboard__key');
 
-
     // create textarea element
     this.output = document.createElement('textarea');
     this.output.classList.add('use-keyboard-input');
@@ -41,11 +40,11 @@ class Keyboard {
     document.body.appendChild(this.output);
 
     // Automatically use keyboard for elements with .use-keyboard-input
-    document.querySelectorAll(".use-keyboard-input").forEach(element => {
-      element.addEventListener("focus", () => {
-          this.open(element.value, currentValue => {
-              element.value = currentValue;
-          });
+    document.querySelectorAll('.use-keyboard-input').forEach((element) => {
+      element.addEventListener('focus', () => {
+        this.open(element.value, (currentValue) => {
+          element.value = currentValue;
+        });
       });
     });
   }
@@ -53,19 +52,17 @@ class Keyboard {
   _createKeys() {
     const fragment = document.createDocumentFragment();
     const keyLayout = [
-      "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-      "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-      "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-      "space"
-    ]
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'backspace',
+      'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+      'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'enter',
+      'done', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '?',
+      'space',
+    ];
 
     // create HTML for an icon
-    const createIconHTML = (icon_name) => {
-      return `<i class="material-icons">${icon_name}</i>`
-    }
+    const createIconHTML = (icon_name) => `<i class="material-icons">${icon_name}</i>`;
 
-    keyLayout.forEach(key => {
+    keyLayout.forEach((key) => {
       const keyElement = document.createElement('button');
       const insertLineBreak = ['backspace', 'p', 'enter', '?'].indexOf(key) !== -1;
 
@@ -74,70 +71,70 @@ class Keyboard {
       keyElement.classList.add('keyboard__key');
 
       switch (key) {
-        case "backspace":
+        case 'backspace':
           keyElement.classList.add('keyboard__key--wide');
           keyElement.innerHTML = createIconHTML('backspace');
 
           keyElement.addEventListener('click', () => {
             this.value = this.value.slice(0, -1);
-            this._triggerEvent('oninput');
-          })
+            this.triggerEvent('oninput');
+          });
 
           break;
 
-        case "caps":
+        case 'caps':
           keyElement.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
           keyElement.innerHTML = createIconHTML('keyboard_capslock');
 
           keyElement.addEventListener('click', () => {
-            this._toggleCapsLock();
+            this.toggleCapsLock();
             keyElement.classList.toggle('keyboard__key--active', this.capsLock);
-          })
+          });
 
           break;
 
-        case "enter":
+        case 'enter':
           keyElement.classList.add('keyboard__key--wide');
           keyElement.innerHTML = createIconHTML('keyboard_return');
 
           keyElement.addEventListener('click', () => {
             this.value += '\n';
-            this._triggerEvent('oninput');
-          })
+            this.triggerEvent('oninput');
+          });
 
           break;
 
-        case "space":
+        case 'space':
           keyElement.classList.add('keyboard__key--extra-wide');
           keyElement.innerHTML = createIconHTML('space_bar');
 
           keyElement.addEventListener('click', () => {
             this.value += ' ';
-            this._triggerEvent('oninput');
-          })
+            this.triggerEvent('oninput');
+          });
 
           break;
 
-        case "done":
+        case 'done':
           keyElement.classList.add('keyboard__key--wide', 'keyboard__key--dark');
           keyElement.innerHTML = createIconHTML('check_circle');
 
           keyElement.addEventListener('click', () => {
             this.close();
-            this._triggerEvent('onclose');
-          })
+            this.triggerEvent('onclose');
+          });
 
           break;
 
-          default:
-            keyElement.textContent = key.toLowerCase();
+        default:
+          keyElement.textContent = key.toLowerCase();
 
-            keyElement.addEventListener("click", () => {
-              this.value += this.capsLock ? key.toUpperCase() : key.toLowerCase();
-              this._triggerEvent("oninput");
-            });
+          keyElement.addEventListener('click', () => {
+            this.value += this.capsLock ? key.toUpperCase() : key.toLowerCase();
+            this.triggerEvent('oninput');
+          });
 
-            break;
+          break;
       }
 
       fragment.appendChild(keyElement);
@@ -150,11 +147,11 @@ class Keyboard {
     return fragment;
   }
 
-  _triggerEvent() {
+  triggerEvent() {
     this.output.value = this.value;
   }
 
-  _toggleCapsLock() {
+  toggleCapsLock() {
     this.capsLock = !this.capsLock;
 
     for (const key of this.keys) {
@@ -165,20 +162,20 @@ class Keyboard {
   }
 
   open(initialValue, oninput, onclose) {
-    this.value = initialValue || "";
+    this.value = initialValue || '';
     this.oninput = oninput;
     this.onclose = onclose;
-    this.main.classList.remove("keyboard--hidden");
+    this.main.classList.remove('keyboard--hidden');
   }
 
   close() {
-    this.value = "";
+    this.value = '';
     this.oninput = oninput;
     this.onclose = onclose;
-    this.main.classList.add("keyboard--hidden");
+    this.main.classList.add('keyboard--hidden');
   }
-};
+}
 
-window.addEventListener("DOMContentLoaded", function () {
-  new Keyboard().init()
+window.addEventListener('DOMContentLoaded', () => {
+  new Keyboard().init();
 });
